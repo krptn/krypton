6ace347532aa3f39e199b5eaa0f5dbd0d28a2fd7
Mark Barsi-Siminszky <mark.barsisiminszky@outlook.com>
2021-04-25 18:27:50 +0200

    x

--- PySec/__init__.py
+++ PySec/__init__.py
@@ -1,9 +1,12 @@
 ﻿from PySec import Basic
 from PySec import decorators
 import hashlib
+import clr
+import sys
 __all__ = ["Basic","decorators"]
 ignore = ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__']
 search = 9
-
+sys.path.append("C:\/Users\/markb\/source\/repos\/PySec\/SecurityDLL\/bin\/Debug\/net5.0")
+clr.AddReference("SecurityDLL")
 def getUser():
     return b"not connected to cloud"
--- SecurityDLL/SecurityDLL.cs
+++ SecurityDLL/SecurityDLL.cs
@@ -3,67 +3,113 @@ using System.IO;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 
-namespace SecurityDLL
+public class HSM
 {
-    public class HSM
+    public HSM()
     {
-        public HSM()
+            
+    }
+}
+
+
+[ComVisible(true)]
+public class helper
+{
+    public helper()
+    {
+
+    }
+
+    [ComVisible(true)]
+    public static Tuple<byte[], byte[]> AESEncrypt(byte[] text, byte[] key)
+    {
+        byte[] encrypted;
+        var handle = GCHandle.Alloc(text, GCHandleType.Pinned);
+        var hand = GCHandle.Alloc(key, GCHandleType.Pinned);
+        // Create an Aes object
+        // with the specified key and IV.
+        byte[] iv;
+        using (Aes myAes = Aes.Create())
+        {
+            iv = myAes.IV;
+        }
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = key;
+            aesAlg.IV = iv;
+
+            // Create an encryptor to perform the stream transform.
+            ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+            // Create the streams used for encryption.
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        //Write all data to the stream.
+                        swEncrypt.Write(text);
+                    }
+                    encrypted = msEncrypt.ToArray();
+                }
+            }
+            aesAlg.Clear();
+        }
+        var r = new Tuple<byte[], byte[]>(encrypted, iv);
+        Array.Clear(text, 0, text.Length);
+        Array.Clear(key, 0, key.Length);
+        handle.Free();
+        hand.Free();
+        text = null;
+        key = null;
+        return r;
+
+    }
+
+    public static String AESDecrypt(Byte[] key, Byte[] thing, Byte[] IV)
+    {
+        var handle = GCHandle.Alloc(key, GCHandleType.Pinned);
+        // Create an Aes object
+        // with the specified key and IV.
+        string plaintext;
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = key;
+            aesAlg.IV = IV;
+
+            // Create a decryptor to perform the stream transform.
+            ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+            // Create the streams used for decryption.
+            using (MemoryStream msDecrypt = new MemoryStream(thing))
+            {
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+
+                        // Read the decrypted bytes from the decrypting stream
+                        // and place them in a string.
+                        plaintext = srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+            aesAlg.Clear();
+        }
+
+        Array.Clear(key, 0, key.Length);
+        handle.Free();
+        key = null;
+
+        try
+        {
+            return plaintext;
+        }
+        finally
         {
             
         }
-    }
-    
-    public class helper
-    {
-        public helper()
-        {
 
-        }
-
-        public static Tuple<byte[], byte[]> AESEncrypt(byte[] text, byte[] key)
-        {
-            byte[] encrypted;
-            var handle = GCHandle.Alloc(text, GCHandleType.Pinned);
-            var hand = GCHandle.Alloc(key, GCHandleType.Pinned);
-            // Create an Aes object
-            // with the specified key and IV.
-            byte[] iv;
-            using (Aes myAes = Aes.Create())
-            {
-                iv = myAes.IV;
-            }
-            using (Aes aesAlg = Aes.Create())
-            {
-                aesAlg.Key = key;
-                aesAlg.IV = iv;
-
-                // Create an encryptor to perform the stream transform.
-                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
-
-                // Create the streams used for encryption.
-                using (MemoryStream msEncrypt = new MemoryStream())
-                {
-                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
-                    {
-                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
-                        {
-                            //Write all data to the stream.
-                            swEncrypt.Write(text);
-                        }
-                        encrypted = msEncrypt.ToArray();
-                    }
-                }
-                aesAlg.Clear();
-            }
-            var r = new Tuple<byte[], byte[]>(encrypted, iv);
-            Array.Clear(text, 0, text.Length);
-            Array.Clear(key, 0, key.Length);
-            handle.Free();
-            hand.Free();
-            text = null;
-            key = null;
-            return r;
-
-        }
     }
 }
