47c468d5243bef67b43f748382057a5dd78d4ae1
mbs9 <66876095+MBS9@users.noreply.github.com>
2021-07-27 18:42:22 +0200

    working in progress

--- Cross-PlatformCryptoLib/Cross-PlatformCryptoLib.cpp
+++ Cross-PlatformCryptoLib/Cross-PlatformCryptoLib.cpp
@@ -25,29 +25,25 @@ extern "C" {
 		*err = *err + 1;
 	}
 		DLLEXPORT int __cdecl AddToStrBuilder(char* buffer,char* content) {
-			if (90 - strlen(buffer) >= strlen(content)) {
-				memcpy_s(buffer, strlen(buffer), content, strlen(content));
-				return 0;
-			}
-			else {
-				return 1;
-			}
-
+			int len = strlen(buffer);
+			memcpy_s(buffer+len, strlen(content), content, strlen(content));
+			return 0;
 		}
 
-		DLLEXPORT unsigned char* __cdecl AESEncrypt(unsigned char* texta, unsigned char* key, char* ivbuff) {
+		DLLEXPORT unsigned char* __cdecl AESEncrypt(unsigned char* text, unsigned char* key, char* ivbuff, char* tag) {
 			int errcnt = 0;
-			int msglen = strlen((char*)texta);
+			int msglen = strlen((char*)text);
+			/*
 			int rem = 16 - remainder(msglen, 16);
-			unsigned char* text = new unsigned char[msglen + (long long)rem];
-			memcpy_s(text, msglen + (long long)rem, texta, msglen);
+			unsigned char* text = new unsigned char[msglen];
+			memcpy_s(text, msglen, texta, msglen);
 			memset(text + msglen, 0, rem);
 			OPENSSL_cleanse(texta, strlen((char*)texta));
-
-			unsigned char iv[16];
-			RAND_bytes(iv, 16);
-			memcpy_s(ivbuff, 16, iv, 16);
-			unsigned char* out = new unsigned char[msglen + (long long)rem];
+			*/
+			unsigned char iv[12];
+			RAND_bytes(iv, 12);
+			memcpy_s(ivbuff, 12, iv, 12);
+			unsigned char* out = new unsigned char[msglen];
 			/*
 			AES_KEY aes_key;
 			AES_set_encrypt_key(key, 256, &aes_key);
@@ -65,29 +61,40 @@ extern "C" {
 			int ciphertext_len;
 			if (!(ctx = EVP_CIPHER_CTX_new()))
 				handleErrors(&errcnt);
-			if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, key, iv))
+			if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, NULL, NULL))
 				handleErrors(&errcnt);
-			if (1 != EVP_EncryptUpdate(ctx, out, &len, text, msglen+rem))
+
+			if (1 != EVP_EncryptInit_ex(ctx, NULL, NULL, key, iv))
+				handleErrors(&errcnt);
+
+			if (1 != EVP_EncryptUpdate(ctx, out, &len, text, msglen))
 				handleErrors(&errcnt);
 			ciphertext_len = len;
 
 			if (1 != EVP_EncryptFinal_ex(ctx, out + len, &len))
+				handleErrors(&errcnt);
+
+			if (1 != EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_GET_TAG, 16, tag))
 				handleErrors(&errcnt);
 			ciphertext_len += len;
 			EVP_CIPHER_CTX_free(ctx);
 
 			OPENSSL_cleanse((void*)text, sizeof((const char*)text));
 			OPENSSL_cleanse((void*)key, sizeof((const char*)key));
-			delete[] text;
-
+			if (errcnt != 0) {
+				unsigned char error[] = "Error";
+				return error;
+			}
 			return out;
 		}
 
-		DLLEXPORT unsigned char* __cdecl AESDecrypt(unsigned char* iv, unsigned char* key, unsigned char* ctexta) {
+		DLLEXPORT unsigned char* __cdecl AESDecrypt(unsigned char* iv, unsigned char* key, unsigned char* ctext, char* tag) {
 			int errcnt = 0;
-			int msglen = strlen((char*)ctexta);
+			int msglen = strlen((char*)ctext);
+			/*
 			unsigned char* ctext = new unsigned char[msglen];
-			memcpy_s(ctext, msglen, ctexta, msglen);
+			memcpy(ctext, ctexta, msglen);
+			*/
 			unsigned char* out = new unsigned char[msglen];
 			/*
 			AES_KEY aes_key;
@@ -98,23 +105,33 @@ extern "C" {
 			int plaintext_len;
 			if (!(ctx = EVP_CIPHER_CTX_new()))
 				handleErrors(&errcnt);
-
-			if (1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, key, iv))
+			if (!EVP_DecryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, NULL, NULL))
 				handleErrors(&errcnt);
-
+			if (!EVP_DecryptInit_ex(ctx, NULL, NULL, key, iv))
+				handleErrors(&errcnt);
+			if (!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, 16, tag))
+				handleErrors(&errcnt);
 			if (1 != EVP_DecryptUpdate(ctx, out, &len, ctext, msglen))
 				handleErrors(&errcnt);
 			plaintext_len = len;
 			if (1 != EVP_DecryptFinal_ex(ctx, out + len, &len))
 				handleErrors(&errcnt);
 			plaintext_len += len;
-
+			int ret = EVP_DecryptFinal_ex(ctx, out + len, &len);
 			
 			EVP_CIPHER_CTX_free(ctx);
 			OPENSSL_cleanse((void*)ctext, sizeof((const char*)ctext));
 			OPENSSL_cleanse((void*)key, sizeof((const char*)key));
 			OPENSSL_cleanse(&iv, sizeof(iv));
-			delete[] ctext;
+
+			if (ret>0) {
+				unsigned char error[] = "Error";
+				return error;
+			}
+			if (errcnt > 0) {
+				unsigned char error[] = "Error";
+				return error;
+			}
 			return out;
 		}
 	
--- PySec/__init__.py
+++ PySec/__init__.py
@@ -15,43 +15,40 @@ elif sys.platform != "win32" and DEBUG:
     a = ctypes.cdll.LoadLibrary(r"Cross-PlatformCryptoLib\out\build\Linux-Clang-Debug\Cross-PlatformCryptoLib.dll")
 else:
     a = ctypes.cdll.LoadLibrary(r"Cross-PlatformCryptoLib\out\build\Linux-Clang-Release\Cross-PlatformCryptoLib.dll")
-"""
-if a.Init() ==0:
-    print("Error")
-"""
 
 Encrypt = a.AESEncrypt
-Encrypt.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p]
+Encrypt.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p,ctypes.c_char_p]
 Encrypt.restype = ctypes.c_char_p
 strbuff = ctypes.create_string_buffer
 NewStrBuilder = ctypes.create_string_buffer
 def RestEncrypt(text, key, keydel = False,condel=False):
     buff = strbuff(text)
-    ctypes.memset(id(text)+32,0,len(text))
-    iv = ctypes.create_string_buffer(16)
+    iv = ctypes.create_string_buffer(12)
     kbuff=strbuff(key)
+    tagbuff = strbuff(16)
     if keydel is True:
         ctypes.memset(id(key)+33,0,len(key)-1)
     if condel is True:
         ctypes.memset(id(text)+33,0,len(text)-1)
-    result = Encrypt(buff, kbuff, iv)
-    a = NewStrBuilder(len(result)+len(iv.value))
+    result = Encrypt(buff, kbuff, iv, tagbuff)
+    a = NewStrBuilder(len(result)+len(iv.value)+1)
     StrAdd(a,result)
     StrAdd(a,iv)
+    StrAdd(a,tagbuff)
     return a.value
 
 Decrypt = a.AESDecrypt
-Decrypt.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p]
+Decrypt.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p]
 Decrypt.restype = ctypes.c_char_p
 
 def RestDecrypt(ctext, key, keydel = False):
-    cbuff = strbuff(ctext[:-16])
+    cbuff = strbuff(ctext[:-12-16])
     kbuff=strbuff(key)
-    iv = ctext[-16:]
-
+    iv = ctext[-12:]
+    tag = ctext[-12-16:-12]
     if keydel is True:
         ctypes.memset(id(key)+33,0,len(key)-1)
-    return Decrypt(iv,kbuff,cbuff)
+    return Decrypt(iv,kbuff,cbuff,strbuff(tag))
 
 __all__ = ["Basic","decorators"]
 ignore = ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__']
