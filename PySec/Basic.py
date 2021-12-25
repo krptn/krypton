c2c8206a30eb9cc8ef39c074c4d871083373f324
mbs9 <66876095+MBS9@users.noreply.github.com>
2021-12-25 18:04:18 +0100

    Fixed code stink

--- PySec/Basic.py
+++ PySec/Basic.py
@@ -28,83 +28,41 @@ def isBaseNameAvailable(self,name:bytes)->bool:
 
 class kms():
     #Needed: update table keys
-    def hmsCipher(self,key):
-        self.key = key
-    def hsmDecipher(self):
-        return self.key
-    def __init__(self, base="defaultBase",tk=Tk(), trust:bool=None, kmsport:bool=None,trustKey:bytes=None)->None:
-        key = os.urandom(32)
+    def secureCipher(self,text,pwdFromUser):
+        pass
+
+    def secureDecipher(self,ctext,pwdFromUser):
+        pass
+
+    def firstUse(self): #For bases only
+        pass
+    def importKeys(self):
+        pass
+
+    def loadFromConfig(self):
+        pass
+
+    def createBase(self):
+        self.c.execute("CREATE TABLE '"+PySec.antiSQLi(self.base)+"' (tbl text, key text)")
+        self.keydb.commit()
+
+
+    def __init__(self, base="defaultBase")->None:
         self.base = base
-        self.root = tk
-        if trust == None:
-            self.trust = messagebox.askyesno(master=self.root, title="Trust?", message="Do you trust this PC and the Microsoft Account linked to it?")
-        else:
-            self.trust = trust
         self.keydb = sqlite3.connect(PySec.key)
         self.c = self.keydb.cursor()
         try:
-            self.c.execute("SELECT * FROM "+PySec.antiSQLi(self.base))
+            self.c.execute("SELECT * FROM "+PySec.antiSQLi(base)) # see if db is set up
         except(sqlite3.OperationalError):
-            if kmsport == None:
-                question = messagebox.askyesno(master=self.root, title="Keys", message="Would you like to import a kms (unsupported)?")
-                if question == False:
-                    self.c.execute("CREATE TABLE '"+PySec.antiSQLi(self.base)+"' (tbl text, key text)")
-                    try:
-                        self.c.execute("INSERT INTO keys VALUES (?,?)",(self.base,key))
-                    except(sqlite3.OperationalError):
-                        self.c.execute("CREATE TABLE keys (base text, key blob)")
-                        self.c.execute("INSERT INTO keys VALUES (?,?)",(self.base,key))
-                    self.keydb.commit()
-                else:
-                    pass
-            else:
-                pass
+            self.firstUse()
 
         self.c.execute("SELECT key FROM keys WHERE db=?",(self.base,))
-        key = self.c.fetchone()[0]
-        if self.trust:
-            self.key = self.hmsCipher(key)
-        else:
-            if trustKey is None:
-                keyr = getKey(self.root)
-                messagebox.showinfo(master = self.root,title="Enter Key", message="Select OK once you have entered the PIN")
-                self.key = PySec.RestEncrypt(key,keyr.value,True)
-            else:
-                self.key = PySec.RestEncrypt(key,trustKey,True)
-
-    def pin(self, rebase:bool=False, kc:sqlite3.Connection=None, base:bool=True, trustKey:bytes=None) -> bytes:
-        if base:
-            base=self.base
-        r = self.c.execute("SELECT key FROM keys WHERE db = ?", (self.base,))
-        r = self.c.fetchone()
-        r = r[0]
-        if self.trust and not rebase:
-            return RestEncrypt(r,self.hsmDecipher(),True)
-        elif self.trust and rebase:
-            r = kc.execute("INSERT INTO keys VALUES (?,?)", (base, RestEncrypt(os.urandom(32),getKey(self.root).value,True,True)))
-        elif not self.trust and not rebase:
-            if trustKey is None:
-                key = getKey(self.root)
-                messagebox.showinfo(master = self.root,title="Enter Key", message="Select OK once you have entered the PIN")
-                key = RestDecrypt(self.key,getKey(self.root).value,False)
-            else:
-                key = RestDecrypt(trustKey,getKey(self.root).value,False)
-            return key
-        elif not self.trust and rebase:
-            raise ValueError("You cannot not trust and recrypt keys simultaniously!")
-        else:
-            raise ValueError("Options contain invalid values!")
+        self.Cipheredkey = self.c.fetchone()[0]
 
     def getTableKey(self, table : str) -> bytes:
         self.c.execute("SELECT key FROM "+PySec.antiSQLi(self.base)+ " WHERE tbl = ?",(table,)) 
-        r = self.c.fetchone()
-        if r == None:
-            self.configTable(self, table)
-            self.c.execute("SELECT key FROM "+PySec.antiSQLi(self.base)+ " WHERE tbl = ?",(table,)) 
-            r = self.c.fetchone()
-        self.c.execute()
-        r = r[0]
-        r = RestDecrypt(r,self.pin(),True)
+        key = self.c.fetchone()[0]
+        r = self.secureDecipher(key)
         return r
 
     def configTable(self,table : (str or bytes)) -> None:
@@ -120,18 +78,15 @@ class kms():
         tmpksys = sqlite3.connect(path)
         bk = tmpkeystore.cursor() #Old
         kc = tmpksys.cursor() #New
-        if pwd == None:
-            pwd = getKey(self.root)
-            messagebox.showinfo(master=self.root, title="Password", message="Please enter the password needed to import the keys on the other machine.")
-            pwd = pwd.value
+
         kc.execute("CREATE TABLE keys (db text, key text)")
         baseCount=0
         for base in bases:
             kc.execute("INSERT INTO keys VLUES (?, ?)", (base, self.pin(rebase=True, kc=kc)))
             for table in tables[baseCount]:
                 key = bk.execute("SELECT key FROM "+PySec.antiSQLi(base[2:])+" WHERE tbl= ?",(table))
-                key = RestDecrypt(key,self.pin(rebase=True, kc=kc, base=base),True,True)
-                key = RestEncrypt(key,self.pin(rebase=True, kc=kc, base=base),True,True)
+                key = self.secureDecipher(key,True)
+                key = self.secureCipher(key,True)
                 kc.execute("INSERT INTO "+PySec.antiSQLi(base)+" VALUES (?, ?)",(table,key))
                 tmpksys.commit()
             baseCount+=1
@@ -149,9 +104,8 @@ class getKey():
         self.b=Button(top,text='Ok',command=self.cleanup)
         self.b.pack()
     def cleanup(self):
-        self.value=self.e.get()
         self.top.destroy()
-        self.value = PySec.getKeyFromPass(self.e.get()) ## This is problamatic.        
+        self.value = PySec.getKeyFromPass(self.e.get())  
 
 
 class crypto(kms):
@@ -160,17 +114,16 @@ class crypto(kms):
     '''
     def __init__(self, baseKMS="{secureStore"):
         super().__init__(base=baseKMS)
+        self.rec = analyzeSecurity)Ö
 
     #Ciphers
     def crypt(self,what:bytes) -> bytes:
-        rec = analyzeSecurity()
-        table = rec.getTableRecommendation()
+        table = self.rec.getTableRecommendation()
         data = RestEncrypt(what,self.getTableKey(table),True,True)
         return data
 
     def decrypt(self,what:bytes)->bytes:
-        rec = analyzeSecurity()
-        table = rec.getTableRecommendation()
+        table = self.rec.getTableRecommendation()
         data = RestDecrypt(what,self.getTableKey(table),True,True)
         return data
 
