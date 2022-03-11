403a7c34273b002a9e8df90d8814dcce721e6007
mbs9 <66876095+MBS9@users.noreply.github.com>
2022-03-11 16:56:17 +0100

    x

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
+from distutils.core import setup
+from distutils.extension import Extension
+import pybind11
 
+setup(name='pysec',
+      version='1.0',
+      description='pysec',
+      author='Mark Barsi-Siminszky',
+      author_email='mark.barsisiminszky@outlook.com',
+      url='https://github.com/mbs9org/PySec',
+      packages=['pysec'],
+      ext_modules=[Extension('pysec.cryptolib', ['CryptoLib/Cryptolib.cpp'],include_dirs=[pybind11.get_include(),"openssl/include"])]
+     )
