6c76b70af815b26a0ff1854e8e597f7d8a87e78e
mbs9 <66876095+MBS9@users.noreply.github.com>
2022-04-24 19:33:02 +0200

    user base class

---
+++ pysec/auth.py
@@ -0,0 +1,26 @@
+from . import basic
+from abc import ABCMeta, abstractmethod
+
+class user(ABCMeta):
+    _userName:str 
+    @property
+    @abstractmethod 
+    def userName(self):
+        pass
+    @userName.setter
+    @abstractmethod
+    def userName(self, newName:str):
+        pass
+    @abstractmethod
+    def save(self):
+        pass
+    @abstractmethod
+    def delete(self):
+        pass
+
+    @abstractmethod
+    def login(self, pwd:str, mfaToken:int|None=None):
+        pass
+    @abstractmethod
+    def logout(self):
+        pass
