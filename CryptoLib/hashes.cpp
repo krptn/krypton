5bb5fb7086bfb00bec11b7dc0629de6867e12f4d
mbs9 <66876095+MBS9@users.noreply.github.com>
2022-07-03 18:42:38 +0200

    this feels better

--- CryptoLib/hashes.cpp
+++ CryptoLib/hashes.cpp
@@ -10,7 +10,7 @@ namespace py = pybind11;
 const auto PBKDF2_HASH_ALGO = EVP_sha512;
 
 py::bytes pyPBKDF2(char* text, int len, char* salt, int iter, int saltLen, int keylen) {
-	//py::gil_scoped_release release;
+	py::gil_scoped_release release;
 	char* key = new char[keylen];
 	int a;
 	a = PKCS5_PBKDF2_HMAC(text, len, (const unsigned char*) salt, saltLen, iter, PBKDF2_HASH_ALGO(), keylen, (unsigned char*)key);
@@ -18,7 +18,7 @@ py::bytes pyPBKDF2(char* text, int len, char* salt, int iter, int saltLen, int k
 	if (a != 1) {
 		throw std::invalid_argument("Unable to hash data.");
 	}
-	//py::gil_scoped_acquire acquire;
+	py::gil_scoped_acquire acquire;
 	return py::bytes(key, keylen);
 }
 
