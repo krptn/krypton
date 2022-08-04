eeefb2da010107a1590beda0aa84cb9dba25dbb5
mbs9 <66876095+MBS9@users.noreply.github.com>
2022-08-04 16:24:35 +0200

    reatempts

--- CryptoLib/OTPs.cpp
+++ CryptoLib/OTPs.cpp
@@ -22,39 +22,34 @@ bool verifyTOTP(py::bytes secret, py::str value) {
     char* key = pymbToBuffer(secret);
     int keylen = secret.attr("__len__")().cast<int>();
     char* code = pyStrToBuffer(value);
-    long int counter = floor(time(NULL)/30);
+    time_t unixTime = floor(time(NULL));
+    unixTime = mktime(gmtime(&unixTime));
+    long int counter = unixTime/30;
     int strCounterLen = 8;
-    unsigned char* strCounter = new unsigned char[strCounterLen];
+    unsigned char strCounter[8];
     //snprintf((char*)strCounter, strCounterLen + 1,"%d", counter);
     int c = 0;
     while (counter != 0) {
-        strCounter[c++] = counter & 0xFF;
+        strCounter[c] = counter;
         counter >>= 8;
+        c++; // The language used :D
     }
-    reverse(strCounter, strCounter+8);
-    cout << "C: " << c;
-    unsigned char* md = new unsigned char[20];
+    //reverse(strCounter, strCounter+8);
+    unsigned char md[20];
     unsigned int len;
-    cout << "Key: " << key << "\n";
-    cout << "Counter: " << strCounter << "\n";
-    HMAC(EVP_sha1(), key, keylen, strCounter, strCounterLen, md, &len);
+    HMAC(EVP_sha1(), key, keylen, (unsigned char*)&strCounter, 8, (unsigned char*)&md, &len);
     OPENSSL_cleanse(key, keylen);
-    cout << "MD: " << md << "\n";
     delete[] key;
-    delete[] strCounter;
     int offset = md[19] & 0xf;
-    cout << "Offset: " << offset << "\n";
     int bin_code = (md[offset] & 0x7f) << 24
 		| (md[offset+1] & 0xff) << 16
 		| (md[offset+2] & 0xff) << 8
 		| (md[offset+3] & 0xff);
     bin_code = bin_code % (int)pow(10, 6);
-    cout << "bin_code: " << bin_code << "\n";
     char correctCode[7];
     snprintf((char*)&correctCode, 7,"%06d", bin_code);
     int compR = compHash(&code, code, 6);
     delete[] code;
-    delete[] md;
     if (compR == 0) {
         return true;
     }
