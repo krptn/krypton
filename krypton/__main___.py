5b79dd498361ba343517d1ef8d52e8cc7e1ce212
mbs9 <66876095+MBS9@users.noreply.github.com>
2022-09-07 19:10:41 +0200

    remove __main__

--- krypton/__main___.py
+++
@@ -1,5 +0,0 @@
-if __name__ == "__main__":
-    # Run tests
-    import os, sys, pathlib
-    os.chdir(pathlib.Path(__file__).parent.parent.as_posix())
-    print(os.system(sys.executable + " -m unittest discover -s tests -p \"*test*.py\" --verbose"))
