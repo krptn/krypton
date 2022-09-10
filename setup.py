d352a3503f2f13945f1ef0cc75cc60afdebb883c
mbs9 <66876095+MBS9@users.noreply.github.com>
2022-09-10 17:49:26 +0200

    there is still a segmentation fault our there

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ class completeDevelop(develop):
     os.chdir(temp)
 
 setup(name='krptn',
-  version='0.1.5',
+  version='0.1.6',
   description='Zero Knowledge security for Python',
   long_description=description,
   long_description_content_type="text/markdown",
