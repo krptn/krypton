0de0e8c5401b1e62bcd194f43f2eac728464cdfc
mbs9 <66876095+MBS9@users.noreply.github.com>
2022-09-11 20:51:20 +0200

    remove uneeded vars

--- CryptoLib/bases.cpp
+++ CryptoLib/bases.cpp
@@ -12,24 +12,15 @@ char* base64(char* data, int length) {
 		pl = 4*((length+(3-(length%3)))/3);
 	}
 	char* output = new char[pl+1];
-	const auto ol = EVP_EncodeBlock(reinterpret_cast<unsigned char *>(output), (const unsigned char*)data, length);
-	output[pl] = '\0';
-	return output;
-}
-
-unsigned char *decode64(char* input, int length) {
-	const auto pl = (length/4)*3;
-	unsigned char* output = new unsigned char[pl+1];
-	const auto ol = EVP_DecodeBlock(output, reinterpret_cast<const unsigned char *>(input), length);
-	output[pl] = '\0';
+	EVP_EncodeBlock(reinterpret_cast<unsigned char *>(output), (const unsigned char*)data, length);
 	return output;
 }
 
 py::bytes py_decode64(const char* input, int length) {
 	const auto pl = (length/4)*3;
 	unsigned char* output = new unsigned char[pl+1];
-	const auto ol = EVP_DecodeBlock(output, reinterpret_cast<const unsigned char *>(input), length);
-	output[pl] = '\0';
+	int out = EVP_DecodeBlock(output, reinterpret_cast<const unsigned char *>(input), length);
+	if (out == -1) handleErrors();
 	py::bytes result = py::bytes((const char*)output, pl).attr("rstrip")(py::bytes("\x00", 1));
 	return result;
 }
