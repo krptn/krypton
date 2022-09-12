162d7ab234e5fb3da0dc1e65ddd1dfedea1244ef
mbs9 <66876095+MBS9@users.noreply.github.com>
2022-09-12 19:49:00 +0200

    add notec

--- setup.py
+++ setup.py
@@ -33,7 +33,10 @@ elif sys.platform == "darwin":
   library_dirs += ["kr-openssl-install/lib"]
   macros += []
   runtime_libs += [os.path.join(folder, "kr-openssl-install/lib")]
-  extra_args += ["-std=c++17"]
+  extra_args += ["-std=c++17", "-O0"]
+
+if sys.platform == "darwin" and not DEBUG:
+  extra_args[1] = "-O1" # Weaken optimizations as they will trigger a segmentation fault
 
 def finishInstall():
   openssl_fips_module = "kr-openssl-install/lib/ossl-modules/fips.dll" if sys.platform == "win32" else "kr-openssl-install/lib64/ossl-modules/fips.so"
