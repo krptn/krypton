acb39d9b459402f5cd90dd705e1e2b3e1ca20d14
mbs9 <66876095+MBS9@users.noreply.github.com>
2022-09-21 20:41:39 +0200

    prepare for v0.1.8

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ class completeDevelop(develop):
     os.chdir(temp)
 
 setup(name='krptn',
-  version='0.1.7',
+  version='0.1.8',
   description='Zero Knowledge security for Python',
   long_description=description,
   long_description_content_type="text/markdown",
