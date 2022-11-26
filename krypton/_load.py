6de297ba54a6e9f12a4f7ff88972e82d4270c825
mbs9 <66876095+MBS9@users.noreply.github.com>
2022-11-26 19:41:10 +0100

    Let's try this

--- krypton/_load.py
+++ krypton/_load.py
@@ -30,10 +30,9 @@ os.environ["OPENSSL_CONF"] = OPENSSL_CONFIG_FILE
 os.environ["OPENSSL_CONF_INCLUDE"] = OPENSSL_CONFIG
 os.environ["OPENSSL"] = OPENSSL_BIN
 
-openssl_fips_module = os.path.join(SITE_PACKAGE, "kr-openssl-install/lib/ossl-modules/fips.dll" if sys.platform == "win32" else "kr-openssl-install/lib64/ossl-modules/fips.so")
-openssl_fips_conf = os.path.join(SITE_PACKAGE, "kr-openssl-config/fipsmodule.cnf")
-openssl = os.path.join(SITE_PACKAGE, 'kr-openssl-install/bin/openssl')
-subprocess.call([openssl, 'fipsinstall', '-out', openssl_fips_conf, '-module', openssl_fips_module])
+OPENSSL_FIPS_MODULE = os.path.join(SITE_PACKAGE, "kr-openssl-install/lib/ossl-modules/fips.dll" if sys.platform == "win32" else "kr-openssl-install/lib64/ossl-modules/fips.so")
+OPENSSL_FIPS_CONF = os.path.join(SITE_PACKAGE, "kr-openssl-config/fipsmodule.cnf")
+subprocess.call([OPENSSL_EXE, 'fipsinstall', '-out', OPENSSL_FIPS_CONF, '-module', OPENSSL_FIPS_MODULE])
 
 if sys.platform == "win32":
     os.add_dll_directory(OPENSSL_BIN)
