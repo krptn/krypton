c83448c7827d4714528128abee7a8ae92a14470d
mbs9 <66876095+MBS9@users.noreply.github.com>
2022-12-25 17:28:44 +0200

    fix naming

--- CryptoLib/CryptoLib.cpp
+++ CryptoLib/CryptoLib.cpp
@@ -70,7 +70,7 @@ void handleErrors() {
 }
 
 PYBIND11_MODULE(__CryptoLib, m) {
-	m.doc() = "Cryptographical component of Krypton. Only for use inside the Krypton module.";
+	m.doc() = "Cryptographical component of Krptn. Only for use inside the Krptn module.";
 	m.def("AESDecrypt", &AESDecrypt, "A function which decrypts the data. Args: text, key.", py::arg("ctext_b"), py::arg("key"));
 	m.def("AESEncrypt", &AESEncrypt, "A function which encrypts the data. Args: text, key.", py::arg("text"), py::arg("key"), py::arg("msglen"));
 	m.def("compHash", &compHash, "Compares hashes", py::arg("a"), py::arg("a"), py::arg("len"));
