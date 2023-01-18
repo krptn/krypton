e723530bfb92c2b093ca68b8ad734018fe12f0e6
mbs9 <66876095+MBS9@users.noreply.github.com>
2023-01-18 18:04:34 +0100

    cleaner code

--- CryptoLib/aes.cpp
+++ CryptoLib/aes.cpp
@@ -94,8 +94,7 @@ py::bytes AESDecrypt(py::bytes ctext_b, py::bytes key){
 	if (!(ret >= 0)) {
 		throw std::invalid_argument("Unable to decrypt ciphertext");
 	}
-	int plainMsgLen = out.get()[3];
-	if (plainMsgLen != '\0') {
+	if (out.get()[3] != '\0') {
 		throw std::invalid_argument("This text was encrypted with older Krptn that is not supported. "
 		"Please do the steps outlined in 13.1 (even though the error is different) on this page: " 
 		"https://docs.krptn.dev/README-FAQ.html#i-get-sqlalchemy-errors-about-missing-columns-tables.");
