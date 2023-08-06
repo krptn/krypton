76d140149c7aa140b7a576fbfc84458a6be6542e
mbs9 <66876095+MBS9@users.noreply.github.com>
2023-08-06 16:26:45 +0200

    differentiate between alpine

--- .setupLocalCIBW.ps1
+++ .setupLocalCIBW.ps1
@@ -1,9 +1,16 @@
 $Env:CIBW_ARCHS="x86_64"
-$Env:CIBW_BEFORE_ALL="yum install -y curl zip unzip tar
-chmod a+x vcpkg/bootstrap-vcpkg.sh
+$Env:CIBW_BEFORE_ALL=@"
+if type apk &> /dev/null; then
+    apk add build-base cmake ninja zip unzip curl git
+    export VCPKG_FORCE_SYSTEM_BINARIES=1
+fi
+if type yum &> /dev/null; then
+    yum install -y curl zip unzip tar
+fi
+chmod a+rx vcpkg/bootstrap-vcpkg.sh
 ./vcpkg/bootstrap-vcpkg.sh
-
-./vcpkg/vcpkg install --triplet x64-linux"
+./vcpkg/vcpkg install --triplet x64-linux
+"@
 $Env:CIBW_PLATFORM="linux"
 $Env:CIBW_TEST_SKIP="pp*"
 $Env:CIBW_BUILD_VERBOSITY=3
